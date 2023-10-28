d300ebefb4aaf716b5abace187cf05e7a2ad8d0a
andres101010 <andreschapu5295@gmail.com>
2023-10-27 20:57:57 -0400

    backAjuste3

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors")
 const dbConnection = require("./dataBase/connection");
 
 var indexRouter = require('./routes/index');
