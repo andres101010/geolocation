2304ca4d0ef9c5a46ebbde414bf3c3317a477de6
andres101010 <andreschapu5295@gmail.com>
2023-10-27 23:39:57 -0400

    front2

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getData = async () => {
     try {
-        const res = await axios.get('http://localhost:3000/geolocation/location')
+        const res = await axios.get('https://geolocation-mfoq.onrender.com/geolocation/location')
         return res
     } catch (error) {
         throw Error(error)
