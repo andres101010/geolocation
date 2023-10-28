b74be5d9eb4894b46d824d5e11c9cf16334f3074
andres101010 <andreschapu5295@gmail.com>
2023-10-27 20:35:26 -0400

    backAjuste2

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+  cors({
+      origin: '*',
+  }),
+);
  //Prueba de conexion de base de datos
 dbConnection.connection.connect((err)=>{
   if (err){
