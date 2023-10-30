b788bf9885b1d4df6653ef3c8dcc17af0fa8763d
andres101010 <andreschapu5295@gmail.com>
2023-10-30 16:03:15 -0400

    mysql2

--- backend/dataBase/connection.js
+++ backend/dataBase/connection.js
@@ -1,7 +1,8 @@
 const dbEnvironment = require("./environment")
-const mysql = require("mysql")
+const mysql = require('mysql2')
 module.exports = {
-    connection: mysql.createConnection({
+    connection: mysql.createPool({
+        connectionLimit: 10,
         host     : dbEnvironment.DB_HOST,
         user     : dbEnvironment.DB_USER,
         password : dbEnvironment.DB_PASSWORD,
